08e2c30503e85cb0a8f7706711fcd689c1dd02b2
arkwis <arkadiusz_wisniewski@yahoo.com>
2015-02-26 14:14:36 -0500

    Use livereload only in dev.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('connect-livereload')());
+if (app.get('env') === 'development') {
+    app.use(require('connect-livereload')());
+}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
