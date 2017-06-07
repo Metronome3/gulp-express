26f3ef3ea71f3bd4b6b31cb512d1cc9797b95e42
arkwis <arkadiusz_wisniewski@yahoo.com>
2017-06-07 13:42:12 -0400

    Update 2017.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Gulp Starter' });
+	res.render('pages/index', { title: 'Gulp Starter' });
 });
 
 module.exports = router;
